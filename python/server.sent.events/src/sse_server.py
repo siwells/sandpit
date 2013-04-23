da9096bdc8389957f761a87e7a308d8e68ca170a
Simon Wells <siwells@gmail.com>
2013-04-23 18:41:57 +0100

    Send JSON formatted data to browser

--- python/server.sent.events/src/sse_server.py
+++ python/server.sent.events/src/sse_server.py
@@ -10,14 +10,19 @@ app = Flask(__name__)
 def event_stream():
     count = 0
     while True:
-        gevent.sleep(2)
+        gevent.sleep(1)
         yield 'data: %s\n\n' % count
         count += 1
+
+def json_stream():
+    while True:
+        gevent.sleep(1)
+        yield  'data: {"id": "John123", "msg":"HELLO WORLD"}\n\n'
 
 @app.route('/my_event_source')
 def sse_request():
     return Response(
-            event_stream(),
+            json_stream(),
             mimetype='text/event-stream')
 
 @app.route('/')
