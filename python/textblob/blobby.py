1428ea3c90eae1cedb1badf93e58df7a3a2a07b1
Simon Wells <siwells@gmail.com>
2016-06-14 00:36:28 +0100

    textblob play

--- python/textblob/blobby.py
+++ python/textblob/blobby.py
@@ -1,9 +1,23 @@
-from textblob import TextBlob
+from textblob import TextBlob, Word
 
 text = '''
-The quick brown fox jumped over the lazy dog
+The quick brown fox jumped over the lazy dog. Pack my box with five dozen liquor jugs
 '''
 
 blob = TextBlob(text)
 print blob.tags
+
+for sentence in blob.sentences:
+    print sentence
+
+for word in blob.sentences[1].words:
+    print Word(word).lemmatize()
+#    print Word(word).pluralize()
+
+#for np in blob.noun_phrases:
+#    print np
+
+#for word, pos in blob.tags:
+#    print word, pos
+
 
