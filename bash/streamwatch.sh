aa6c9abf24f0945681866ee7dc4fffe507fbcc56
Simon Wells <siwells@gmail.com>
2020-08-19 13:48:28 +0100

    Add script for simultaneous saving and viewing of a streamed broadcast, e.g. from BBC (or anything that works with youtube-dl)

---
+++ bash/streamwatch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Both watch and save a broadcast stream at the same time
+# Assumes both youtube-dl and mpv are installed.
+# NB. Mpv can be substituted for any other media player
+# that accepts a stdstream input from the shell, e.g. mplayer
+
+URL=$1
+TITLE=$(youtube-dl --skip-download --get-title --no-warnings $URL 
+    | sed 2d 
+    | awk '{print tolower($0)}')
+youtube-dl $URL -o - | tee "$TITLE".mp4 | mpv -
+
